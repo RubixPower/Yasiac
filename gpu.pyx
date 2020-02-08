80314738ac56d91c5d4780dbec33ccd48879406b
rubixpower <matej@starc.dev>
2020-02-08 22:07:57 +0100

    Some variable typing and cleaner code

--- gpu.pyx
+++ gpu.pyx
@@ -6,64 +6,56 @@ cdef class Gpu():
         self.sensors = subprocess.getoutput("sensors -A").splitlines()
         self.glxinfo = subprocess.getoutput("glxinfo -B").splitlines()
         self.amdgpu_info = subprocess.getoutput("cat /sys/kernel/debug/dri/0/amdgpu_pm_info").splitlines()
+
     cpdef str name(self):
-        cdef list data
         cpdef str line
-        cpdef str name
         for line in self.glxinfo:
             if 'Device: ' in line:
-                name = line.split('Device: ')[1].replace('(TM) ', '').split(' (')[0]
-                return name
+                return line.split('Device: ')[1].replace('(TM) ', '').split(' (')[0]
 
     cpdef int vram_total(self):
         cdef int vram
         cdef str line
         for line in self.glxinfo:
             if 'Video memory:' in line:
-                vram = int(line.split(': ')[1].replace('MB', '').strip())
-                return vram
+                return int(line.split(': ')[1].replace('MB', '').strip())
                 
-    cpdef int vram_usage_percentage(self):
+    cpdef str vram_usage_percentage(self):
         cdef list data
-        cdef int vram_usage
         data = (subprocess.getoutput("rocm-smi --showmemuse")).splitlines()
         for line in data:
             if 'GPU memory use (%)' in line:
-                vram_percentage = line.split('GPU memory use (%):')[-1].strip()
-                return int(vram_percentage)
+                return line.split('GPU memory use (%):')[-1].strip()
                 
-
     cpdef str vram_usage_total(self):
         cdef int vram_total, vram_usage_percentage, vram_usage
         cdef str vram_usage_total
-        vram_total = int(self.vram_total())
+        vram_total = self.vram_total()
         vram_usage_percentage = int(self.vram_usage_percentage())
         one_percentage = vram_total / 100
-        usage = str(int(one_percentage * vram_usage_percentage))
-        vram_usage_total = (f'~{usage}/{str(vram_total)}')
-        return vram_usage_total
+        usage = round(one_percentage * vram_usage_percentage)
+        return (f'~{usage}/{vram_total}')
 
     cpdef str clock(self):
+        cdef str line
         for line in self.amdgpu_info:
-            if '(SCLK)' in line:
-                clock = line.split(' MHz (SCLK)')[0].strip()
-        return clock
+            if '(SCLK)' in line: 
+                return f"{line.split(' MHz (SCLK)')[0].strip()} [MHz]"
 
-    cpdef int fan_speed_current(self):
+    cpdef str fan_speed_current(self):
         cdef str line
         for line in self.sensors:
             if 'fan1:' in line:
-                FanSpeed = line.split('fan1:')[1].split('  (')[0].strip().replace('RPM', '')
-                return int(FanSpeed)
+                return line.split('fan1:')[1].split('  (')[0].strip()
+
     cpdef str temperature(self):
+        cdef str line
         for line in self.amdgpu_info:
             if 'GPU Temperature:' in line:
-                temp = line.split('GPU Temperature: ')[1].replace(' C', ' °C')
-        return temp
+                return line.split('GPU Temperature: ')[1].replace(' C', ' °C')
 
     cpdef str load(self):
-        cdef str load
+        cdef str line
         for line in self.amdgpu_info:
             if 'GPU Load:' in line:
-                load = line.split(': ')[1]
-                return load
+                return line.split(': ')[1]
